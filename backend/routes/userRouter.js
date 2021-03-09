533ae326599523c1f0a6bca23de78ba4902a4561
SubhoBasak <subhobasak50@gmail.com>
2021-03-09 11:58:02 +0530

    change post to get

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,23 +1,36 @@
 // exports
-const express = require('express');
-const userController = require('../controllers/userController');
+const express = require("express");
+const userController = require("../controllers/userController");
 const router = express.Router();
 
 const ensureToken = (req, res, next) => {
-	const header = req.headers['authorization'];
-	if (typeof header !== 'undefined') {
-		req.token = header;
-		next();
-	} else {
-		res.sendStatus(403);
-	}
+  const header = req.headers["authorization"];
+  if (typeof header !== "undefined") {
+    req.token = header;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
 };
 
-router.post('/login', userController.login);
-router.post('/sign-up', userController.signUp);
-router.post('/completed/exams', ensureToken, userController.getCompletedExamList);
-router.get('/completed/exam', ensureToken, userController.getCompletedExamById);
-router.post('/incomplete/exams', ensureToken, ensureToken, userController.getIncompleteExamList);
-router.get('/incomplete/exam', ensureToken, userController.getIncompleteExamById);
+router.post("/login", userController.login);
+router.post("/sign-up", userController.signUp);
+router.get(
+  "/completed/exams",
+  ensureToken,
+  userController.getCompletedExamList
+);
+router.get("/completed/exam", ensureToken, userController.getCompletedExamById);
+router.get(
+  "/incomplete/exams",
+  ensureToken,
+  ensureToken,
+  userController.getIncompleteExamList
+);
+router.get(
+  "/incomplete/exam",
+  ensureToken,
+  userController.getIncompleteExamById
+);
 
 module.exports = router;
