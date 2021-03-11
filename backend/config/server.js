ea48c900ff6a3b2611f883298499be5b78c4363d
AbinashBasak <abinashbasak129@gmail.com>
2021-03-12 00:58:03 +0530

    fetch exam questions

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,5 +1,5 @@
 module.exports = {
 	MONGO_URI: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/examPortal',
-	PORT: process.env.PORT || 3000,
-	ENCRYPTION_KEY: process.env.ENCRYPTION_KEY || 'asdfghjklzxcvbnmqwertyuiopasdfgh',
+	PORT: process.env.PORT || 5000,
+	ENCRYPTION_KEY: 'asdfghjklzxcvbnmqwertyuiopasdfgh',
 };
